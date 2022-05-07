fbfca52742b7a4518247153bddf53ed9bd3c7f65
maoxiaoke@outlook.com <maoxiaoke@outlook.com>
2022-05-07 10:42:19 +0800

    conflict a

---
+++ .changeset/lazy-humans-jump.md
@@ -0,0 +1,5 @@
+---
+"@xkmao/nazha-monorepo-b": major
+---
+
+turn 3 to 4 using major version
--- packages/b/index.js
+++ packages/b/index.js
@@ -1,7 +1,7 @@
 import { add } from '@xkmao/nazha-monorepo-a';
 
 const multi = a => {
-  return add(3) * a;
+  return add(4) * a;
 }
 
 export { multi }
